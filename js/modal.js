0d619e9d03bcb283e8041c8dd6360f3cd55ed76d
ZenatiSarah <62511328+ZenatiSarah@users.noreply.github.com>
2023-07-12 18:57:43 +0200

    fermeture du modal réussi

--- js/modal.js
+++ js/modal.js
@@ -9,13 +9,20 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const closeBtn = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.addEventListener('click', closeModal);
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+function closeModal() {
+  modalbg.style.display = "none";
 }
